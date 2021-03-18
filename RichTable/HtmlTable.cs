26bf938a68aefc5d3740fbf06c2be4bcfa84e83f
zmjack <jack@zmland.com>
2021-03-19 06:04:07 +0800

    v0.1.1

--- RichTable/HtmlTable.cs
+++ RichTable/HtmlTable.cs
@@ -34,8 +34,8 @@ namespace Richx
             var dict = new Dictionary<string, string>();
             var style = cell.Style;
 
-            if (style.BackgroundColor is not null) dict.Add("background-color", $"#{style.BackgroundColor.Red:x2}{style.BackgroundColor.Green:x2}{style.BackgroundColor.Blue:x2}{style.BackgroundColor.Alpha:x2}");
-            if (style.Color is not null) dict.Add("color", $"#{style.Color.Red:x2}{style.Color.Green:x2}{style.Color.Blue:x2}{style.Color.Alpha:x2}");
+            if (style.BackgroundColor is not null) dict.Add("background-color", $"#{style.BackgroundColor.Red:x2}{style.BackgroundColor.Green:x2}{style.BackgroundColor.Blue:x2}{(style.BackgroundColor.Alpha < byte.MaxValue ? $"{style.BackgroundColor.Alpha:x2}" : "")}");
+            if (style.Color is not null) dict.Add("color", $"#{style.Color.Red:x2}{style.Color.Green:x2}{style.Color.Blue:x2}{(style.Color.Alpha < byte.MaxValue ? $"{style.Color.Alpha}:x2" : "")}");
             if (style.FontFamily is not null) dict.Add("font-family", $"{style.FontFamily}");
             if (style.FontSize.HasValue) dict.Add("font-size", $"{style.FontSize}px");
             if (style.Bold.HasValue && style.Bold.Value) dict.Add("font-weight", $"bold");
