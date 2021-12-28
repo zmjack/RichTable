ded54f2d4be77ffd145bc11d928e2020561d0d49
zmjack <jack@zmland.com>
2021-12-28 18:14:14 +0800

    v0.2.35

--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
 nuget push "Richx/bin/Release/Richx.0.1.4.nupkg" -source nuget.org
-nuget push "RichTable/bin/Release/RichTable.0.2.34.nupkg" -source nuget.org
+nuget push "RichTable/bin/Release/RichTable.0.2.35.nupkg" -source nuget.org
 pause
